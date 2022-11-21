a5514e4adcaf6ce441770cba6f6ebd93dc915633
Dumilde Matos <dumildejosematos@gmail.com>
2022-11-21 20:00:30 +0000

    first commit

---
+++ src/test/java/com/app/apimymental/ApiMymentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.apimymental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMymentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
